cfdb968b45b313a13c342f3071b03abcdafed386
JonnyEst <jonnya.estupinanf@utadeo.edu.co>
2022-08-29 19:41:42 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Numbers numbers = new Numbers();
+
+        System.out.println("Digite un número");
+        
+
+    }
+}
---
+++ src/Numbers.java
@@ -0,0 +1,22 @@
+public class Numbers {
+
+    private int number;
+
+
+    public Numbers() {
+    }
+
+    public int getNumber() {
+        return number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Numbers{" +
+                "number=" + number +
+                '}';
+    }
+}
