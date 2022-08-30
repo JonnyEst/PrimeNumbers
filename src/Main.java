424f4e8af69abb6d615d37e531ce76ff5d087e2a
JonnyEst <jonnya.estupinanf@utadeo.edu.co>
2022-08-29 19:54:28 -0500

    The method is defined

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,23 @@ public class Main {
         Numbers numbers = new Numbers();
 
         System.out.println("Digite un número");
-        
+        numbers.setNumber(input.nextInt());
 
+        prime(numbers.getNumber());
+
+
+    }
+
+    public static boolean prime(int number){
+        int contador =2;
+        boolean prime=true;
+        while((prime) && (contador!=number)){
+            if(number % contador==0)
+                prime = false;
+                System.out.println("Is not prime");
+                contador++;
+        }
+        System.out.println("Is prime");
+        return prime;
     }
 }
